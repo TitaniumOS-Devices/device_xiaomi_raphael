92cb600ec0433154f881301b577945318c70925f
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-02 16:28:55 -0300

    raphael: Use Coral FP to bypass safetynet
    Change-Id: I3545cb4d0e96d08493445462a55cb230ac498692
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -34,28 +34,33 @@ void property_override(char const prop[], char const value[])
     else
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
+
+void property_override_dual(char const system_prop[], char const vendor_prop[],
+    char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 void load_raphaelglobal() {
     property_override("ro.product.model", "Mi 9T Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 9 PKQ1.181121.001 V10.3.1.0.PFKEUXM release-keys");
-    property_override("ro.build.fingerprint", "Xiaomi/raphael_eea/raphael:9/PKQ1.181121.001/V10.3.1.0.PFKEUXM:user/release-keys");
+    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.4.0.QFKMIXM release-keys");
 }
 
 void load_raphaelin() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphaelin");
     property_override("ro.product.device", "raphaelin");
-    property_override("ro.build.description", "raphaelin-user 9 PKQ1.181121.001 V10.3.3.0.PFKINXM release-keys");
-    property_override("ro.build.fingerprint", "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys");
+    property_override("ro.build.description", "raphaelin-user 10 QKQ1.190825.002 V11.0.4.0.QFKINXM release-keys");
 }
 
 void load_raphael() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "raphael-user 9 PKQ1.181121.001 V10.3.12.0.PFKCNXM release-keys");
-    property_override("ro.build.fingerprint", "Xiaomi/raphael/raphael:9/PKQ1.181121.001/V10.3.12.0.PFKCNXM:user/release-keys");
+    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.8.0.QFKCNXM release-keys");
 }
 
 
@@ -72,4 +77,5 @@ void vendor_load_properties() {
         LOG(ERROR) << __func__ << ": unexcepted region!";
     }
 
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys");
 }
