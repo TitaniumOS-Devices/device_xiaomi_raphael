954fed65a6e8d1ce26f8c023867e9b749ba19e08
UtsavisGreat <utsavbalar1231@gmail.com>
2019-11-11 03:33:34 +0100

    raphael: override privapp permissions to log
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -77,5 +77,6 @@ void vendor_load_properties() {
         LOG(ERROR) << __func__ << ": unexcepted region!";
     }
 
+    property_override("ro.control_privapp_permissions", "log");
     property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys");
 }
