a12b7fb971346698d69bcff5cff854fe26246d92
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-18 18:44:03 +0000

    raphael: include customized version of ANX Camera
    * Also Enable TARGET_USES_PREBUILT_CAMERA_SERVICE
    
    Required to fix 48MP and portrait mode on miui camera
    
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- titanium_raphael.mk
+++ titanium_raphael.mk
@@ -24,3 +24,6 @@ BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFK
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_GAPPS_ARCH := arm64
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
