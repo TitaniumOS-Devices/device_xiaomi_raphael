aa6193aa330dd9604e73ec891ae70cdf6605efc0
Nauval Rizky <enuma.alrizky@gmail.com>
2020-02-23 14:05:12 +0900

    raphael: introduce gfscreenoffd
    This is a small daemon to capture fingerprint touch event while screen is off.
    goodix_ts fire a specific input event when the area of fingerprint
    is being touched on screen off but fod_status is on.
    This daemon will create an another uinput and act as a virtual input
    based on goodix_ts's specific screen off event.
    
    Inspired from Okitavera's FOD module for Mi 9 SE and TheScarastic's
    onPress() IFingerprintInscreenCallback in reversed SystemUI.
    
    raphael: gfscreenoffd: only write fod_status if it was disabled
    
    raphael: gfscreenoffd: rely startup completely on DeviceKeyHandler
    
    raphael: gfscreenoffd: ignore other ops when screen is already on
    
    raphael: gfscreenoffd: add missing LOG_TAG
    
    raphael: gfscreenoffd: simplify touchscreen searching
    
    raphael: gfscreenoffd: skip time checking on first touch
    
    raphael: gfscreenoffd: optimize listener
    
    * separate fod_status enforcement and adjust code-style for readability
    * use pool instead pointer reposition for framebuffer checking
    
    raphael: gfscreenoffd: runtime and code style improvements
    
    raphael: gfscreenoffd: fixup cpu regression
    
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- device.mk
+++ device.mk
@@ -71,7 +71,8 @@ PRODUCT_COPY_FILES += \
     vendor/titanium/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 PRODUCT_PACKAGES += \
-    lineage.biometrics.fingerprint.inscreen@1.1-service.raphael
+    lineage.biometrics.fingerprint.inscreen@1.1-service.raphael \
+    gfscreenoffd
 
 # FM
 PRODUCT_PACKAGES += \
