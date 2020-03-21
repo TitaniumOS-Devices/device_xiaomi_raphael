870f80c6da694efd6fec5fe375b178a9c5885f0e
Saikat Halder <iamdevsaikat@gmail.com>
2020-03-21 14:18:31 +0530

    raphael: Bringup for TitaniumOS
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- titanium_raphael.mk
+++ titanium_raphael.mk
@@ -1,19 +1,22 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 The TitaniumOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common TitaniumOS stuff.
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TITANIUM_BUILD_TYPE := OFFICIAL
+$(call inherit-product, vendor/titanium/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := titanium_raphael
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
