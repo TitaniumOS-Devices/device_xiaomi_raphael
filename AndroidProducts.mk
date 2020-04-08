126977a7e20c77bf85cd9d7f79e78fa8ca409596
Saikat Halder <iamdevsaikat@gmail.com>
2020-04-08 19:53:41 +0000

    raphael: Bringup for TitaniumOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_raphael.mk
+    $(LOCAL_DIR)/titanium_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    titanium_raphael-user \
+    titanium_raphael-userdebug \
+    titanium_raphael-eng
--- titanium_raphael.mk
+++ titanium_raphael.mk
@@ -6,15 +6,21 @@
 
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Titanium stuff.
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TITANIUM_BUILD_TYPE := OFFICIAL
+TARGET_INCLUDE_WIFI_EXT := true
+$(call inherit-product, vendor/titanium/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := titanium_raphael
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_GAPPS_ARCH := arm64
